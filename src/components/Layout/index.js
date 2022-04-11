1b128788a7ceae031009f5041b963fdbe631eb9a
yuhueilee <ylee0026@student.monash.edu>
2022-04-11 11:42:13 +0800

    Add styles and display html tag

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,7 +2,19 @@ import SideBar from '../SideBar';
 import './index.scss';
 
 const Layout = () => {
-  return <><SideBar /></>
+  return (
+    <div className="App">
+      <SideBar />
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default Layout;
