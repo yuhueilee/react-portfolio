1f578444fca02dc9849fdc12fed2fd4e5a77e24a
yuhueilee <ylee0026@student.monash.edu>
2022-04-11 11:16:53 +0800

    Configure the logo and subtitle image and styles

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
 import './index.scss';
 import LogoW from '../../assests/images/logoW.png'
+import LogoSubtitle from '../../assests/images/logoSubtitle.png'
 
 const SideBar = () => {
   return (
-    <div className='nav-bar'>
-      <Link className='logo' to='/'>
+    <div className="nav-bar">
+      <Link className="logo" to='/'>
         <img src={LogoW} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="wendy" />
       </Link>
     </div>
   );
