843752b5f9c4239019fe084d53f519c6b06956e6
yuhueilee <ylee0026@student.monash.edu>
2022-04-11 16:03:54 +0800

    Add About component and its styles

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,79 @@
-import './index.scss';
+import { useEffect, useState } from 'react'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './index.scss'
 
 const About = () => {
-  return (
-    <div>About</div>
-  );
-};
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-export default About;
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm a freash graduate from Monash University Malaysia looking for a job position
+            in web/software development. 
+          </p>
+          <p align="LEFT">
+            I'm self-disciplined, well-organized, and a fast-learner who always try to improve
+            by exploring new technologies and enhancing my skills. I also enjoy working as a team 
+            and communicating with my team members.
+          </p>
+          <p>
+            Throughout the study, I have participated in a few programming competitions as well as sports club.
+            Additionally, I worked as a front-end intern at a Malaysia IT company for 5 months. Not only
+            did I learn different web technologies, but also become more confident in presentaion.
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default About
