36ae4e56ddcbc9dc134de65f310e576183ac247d
yuhueilee <ylee0026@student.monash.edu>
2022-04-11 14:43:54 +0800

    Add AnimatedLetters component to apply animation to characters, apply style class through useEffect

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,57 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 import LogoTitle from '../../assests/images/logoW.png'
-import { Link } from 'react-router-dom';
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = ['e', 'n', 'd', 'y']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
 
   return (
     <div className='container home-page'>
       <div className='text-zone'>
         <h1>
-          Hi, <br /> I'm
-          <img src={LogoTitle} alt='developer' />
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
           <br />
-          web developer
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={LogoTitle} alt='developer' />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={19}
+          />
         </h1>
         <h2>Fresh Graduate / Front-end Developer</h2>
         <Link to='/contact' className='flat-button'>CONTACT ME</Link>
