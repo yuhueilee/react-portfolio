d62a040b413639bb74da864302b6107b240f69de
yuhueilee <ylee0026@student.monash.edu>
2022-04-11 11:31:35 +0800

    Add icons for home, about, contact, github and linkedin

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,4 +1,7 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import './index.scss';
 import LogoW from '../../assests/images/logoW.png'
 import LogoSubtitle from '../../assests/images/logoSubtitle.png'
@@ -10,6 +13,41 @@ const SideBar = () => {
         <img src={LogoW} alt="logo" />
         <img className="sub-logo" src={LogoSubtitle} alt="wendy" />
       </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/yuhueilee-wendy/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/yuhueilee"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
