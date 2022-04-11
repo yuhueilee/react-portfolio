6ee3345ff5dd29306bc3d5afc60aee47927beed3
yuhueilee <ylee0026@student.monash.edu>
2022-04-11 14:21:13 +0800

    Add content to Home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,23 @@
 import './index.scss';
+import LogoTitle from '../../assests/images/logoW.png'
+import { Link } from 'react-router-dom';
 
-const Home = () => ('Hello');
+const Home = () => {
+
+  return (
+    <div className='container home-page'>
+      <div className='text-zone'>
+        <h1>
+          Hi, <br /> I'm
+          <img src={LogoTitle} alt='developer' />
+          <br />
+          web developer
+        </h1>
+        <h2>Fresh Graduate / Front-end Developer</h2>
+        <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
