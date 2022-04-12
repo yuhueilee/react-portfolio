e2133ac3394b97377175adfc18817db66a31dd5e
yuhueilee <ylee0026@student.monash.edu>
2022-04-12 15:15:18 +0800

    Add base property to HashRouter tag

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { HashRouter } from 'react-router-dom';
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <HashRouter>
+  <HashRouter base="/">
     <App />
   </HashRouter>
 );
