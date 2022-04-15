b4430473b7d859a9ae4d919f93c0ae7ef4376a99
yuhueilee <ylee0026@student.monash.edu>
2022-04-15 10:46:59 +0800

    Add heading

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import './index.scss';
+import Loader from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Projects = () => {
-  return <div></div>;
+  const [letterClass, setLetterClass] = useState('text-animate');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, []);
+
+  return (
+    <>
+      <div className='container project-page'>
+        <div className='text-zone'>
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['P', 'r', 'o', 'j', 'e', 'c', 't', ' ', 'E', 'x', 'p', 'e', 'r', 'i', 'e', 'n', 'c', 'e', 's']}
+              idx={15}
+            />
+          </h1>
+        </div>
+      </div>
+      <Loader type='line-scale-pulse-out' />
+    </>
+  );
 }
 
 export default Projects;
