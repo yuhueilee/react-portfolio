7554413a394a9400216f4bad8b778a12b7d19fb6
yuhueilee <wendylee70127@gmail.com>
2024-02-21 17:22:40 +0800

    Use browser router to render app component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <HashRouter base="/">
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
